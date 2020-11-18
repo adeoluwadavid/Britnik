c3946a2b152d84a997da51421969f90a98ef1c9c
adeoluwadavid <adeoluwadavid@gmail.com>
2020-11-18 06:47:51 +0100

    second-commit

--- src/components/page1.js
+++ src/components/page1.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import '../css/page1.css'
-import {Image} from 'react-bootstrap'
+import repair from '../images/car-repair.svg'
+import servicing from '../images/car-wash-car.svg'
+import overhaul from '../images/car-overhaul.svg'
+
+import {Image,Card, CardDeck,Collapse, Carousel} from 'react-bootstrap'
 function Page1(){
-    const display = ()=>{
-        document.getElementById('repair_tools_item').style.display = 'block'
-    }
-    const display2 = ()=>{
-        document.getElementById('repair_tools_item2').style.display = 'block'
-    }
+    const [open, setOpen] = React.useState(false);
+    const [close,setClose] = React.useState(false)
     return(
         <div>
             <div id="engr_container">
@@ -39,28 +39,89 @@ function Page1(){
                                 Our goal is to always put the client first.
                             </p>
                         </div>
-                        <div id="repair_tools" onClick={display}>
+                        <div id="repair_tools" onClick={()=>setOpen(!open)}>
                             <h5>SUSTAINABLE REPAIR</h5>
-                            <p id="repair_tools_item">
+                            <Collapse in={open}>
+                                <div id="example-collapse-text">
                                 We listen when our clients talk.
                                 Our goal is to always put the client first.
-                            </p>
+                                </div>
+                            </Collapse>
                         </div>
-                        <div id="repair_tools" onClick={display2}>
+                        <div id="repair_tools" onClick={()=> setClose(!close)}>
                             <h5>USAGE OF MODERN TOOLS</h5>
-                            <p id="repair_tools_item2">
+                            <Collapse in={close}>
+                                <div id="example-collapse-text">
                                 We listen when our clients talk.
                                 Our goal is to always put the client first.
-                            </p>
-                        </div>
-                        <div>
-
+                                </div>
+                            </Collapse>
                         </div>
                     </div>
                 </div>
             </div>
             <div id="safety_et_al">
                 SAFETY. QUALITY. TIMELY DELIVERY
+            </div>
+            <div>
+                <h5 id="what_we_do">WHAT WE DO</h5>
+                <CardDeck id="flex">
+                    <Card>
+                        <Card.Img variant="top" src={repair} id="svg" />
+                        <Card.Body>
+                        <Card.Title id="what_we_do">REPAIRS</Card.Title>
+                        <Card.Text>
+                            Lorem Ipsum is simply dummy text 
+                            of the printing and typesetting industry. 
+                            Lorem Ipsum has been the industry's standard
+                        </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <Card>
+                        <Card.Img variant="top" src={servicing} id="svg"/>
+                        <Card.Body>
+                        <Card.Title id="what_we_do">SERVICING</Card.Title>
+                        <Card.Text>
+                            Lorem Ipsum is simply dummy text 
+                            of the printing and typesetting industry. 
+                            Lorem Ipsum has been the industry's standard
+                        </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <Card>
+                        <Card.Img variant="top" src={overhaul} id="svg"/>
+                        <Card.Body>
+                        <Card.Title id="what_we_do">VEHICLE OVERHAUL</Card.Title>
+                        <Card.Text>
+                            Lorem Ipsum is simply dummy text 
+                            of the printing and typesetting industry. 
+                            Lorem Ipsum has been the industry's standard
+                        </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </CardDeck>
+            </div>
+            <div>
+                <h5 id="from_our_clients">FROM OUR CLIENTS</h5>
+                <Carousel id="from_our_clients_carousel">
+                    <Carousel.Item>
+                        <h5>We enjoy Britnik Engineering Service</h5>
+                        <p>Seun Ogegbe</p>
+                        <Carousel.Caption>
+                        <h5>Engr Seun's service is the best</h5>
+                         <p>Segun Agbaje</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                     
+                    <Carousel.Item>
+                         <h5>Engr Seun's service is the best</h5>
+                         <p>Segun Agbaje</p>
+                         <Carousel.Caption>
+                         <h5>Engr Seun's service is the best</h5>
+                         <p>Segun Agbaje</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
             </div>
         </div>
     )
